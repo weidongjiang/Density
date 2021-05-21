a4b25432243e8972ae94c7d047ab6ad793f3dfeb
wheatear <wheatear.jiang@qiffa.com>
2021-05-21 16:25:49 +0800

    appdele

--- Density/Density/AppDelegate.m
+++ Density/Density/AppDelegate.m
@@ -6,16 +6,26 @@
 //
 
 #import "AppDelegate.h"
+#import "MDRootViewController.h"
+#import "MDBaseNavigationController.h"
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-
+@synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    self.window.backgroundColor = [UIColor whiteColor];
+    MDBaseNavigationController *vc = [MDRootViewController appRootViewController];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    
+    [UIApplication sharedApplication].statusBarHidden = NO;
+    
     return YES;
 }
 
