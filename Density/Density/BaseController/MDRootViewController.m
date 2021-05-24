f7943ee889a5af2f41908dc17a5f1ab80d4f40d2
wheatear <wheatear.jiang@qiffa.com>
2021-05-24 11:11:18 +0800

    添加图片

--- Density/Density/BaseController/MDRootViewController.m
+++ Density/Density/BaseController/MDRootViewController.m
@@ -25,24 +25,24 @@
     MDBaseTabBarController *tabBarController = [[MDBaseTabBarController alloc] init];
     
     MDChatViewController *chatVC = [[MDChatViewController alloc] init];
-    UIImage *tabBarItem_chatVC_image = [UIImage imageNamed:@"icon_tabbar_01"];
-    UIImage *tabBarItem_chatVC_selectedImage = [UIImage imageNamed:@"icon_tabbar_01_s"];
+    UIImage *tabBarItem_chatVC_image = [UIImage imageNamed:@"Density_icon_tabbar_chat"];
+    UIImage *tabBarItem_chatVC_selectedImage = [UIImage imageNamed:@"Density_icon_tabbar_chat_s"];
     chatVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"" image:[tabBarItem_chatVC_image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[tabBarItem_chatVC_selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     chatVC.tabBarItem.badgeValue = @"9";
     chatVC.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0);
     
     
     MDRecordViewController *recordVC = [[MDRecordViewController alloc] init];
-    UIImage *tabBarItem_recordVC_image = [UIImage imageNamed:@"icon_tabbar_02"];
-    UIImage *tabBarItem_recordVC_selectedImage = [UIImage imageNamed:@"icon_tabbar_02_s"];
+    UIImage *tabBarItem_recordVC_image = [UIImage imageNamed:@"Density_icon_tabbar_record"];
+    UIImage *tabBarItem_recordVC_selectedImage = [UIImage imageNamed:@"Density_icon_tabbar_record_s"];
     recordVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"" image:[tabBarItem_recordVC_image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[tabBarItem_recordVC_selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     recordVC.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0);
     
     MDWordViewController *wordVC = [[MDWordViewController alloc] init];
-    UIImage *tabBarItem_wordVC_image = [UIImage imageNamed:@"icon_tabbar_03"];
-    UIImage *tabBarItem_wordVC_selectedImage = [UIImage imageNamed:@"icon_tabbar_03_s"];
+    UIImage *tabBarItem_wordVC_image = [UIImage imageNamed:@"Density_icon_tabbar_word"];
+    UIImage *tabBarItem_wordVC_selectedImage = [UIImage imageNamed:@"Density_icon_tabbar_word_s"];
     wordVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"" image:[tabBarItem_wordVC_image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[tabBarItem_wordVC_selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-    wordVC.tabBarItem.badgeValue = @"9";
+    wordVC.tabBarItem.badgeValue = @"99";
     wordVC.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0);
     
     tabBarController.viewControllers = @[chatVC,recordVC,wordVC];
